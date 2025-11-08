13081e7dce25898af0639c1dd4d48ea1d6364b9c
Rohit Kumar <contactme.rohitkumar@gmail.com>
2025-11-08 19:27:07 +0530

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,82 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {Provider} from 'react-redux'
+import store from './store/store.js'
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
+import Home from './components/pages/Home.jsx'
+import Services from './components/pages/Services.jsx'
+import Portfolio from './components/pages/Portfolio.jsx'
+import About from './components/pages/About.jsx'
+import Contact from './components/pages/Contact.jsx'
+import Testimonials from './components/pages/Testimonials.jsx'
+import Events from './components/pages/Events.jsx'
+import PrivacyPolicy from './components/pages/PrivacyPolicy.jsx'
+import TermsConditions from './components/pages/TermsConditions.jsx'
+import Support from './components/pages/Support.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/services",
+        element: <Services/>,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio/>,
+      },
+      {
+        path: "/about",
+        element: <About/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>,
+      },
+      {
+        path: "/testimonials",
+        element: <Testimonials/>,
+      },
+      {
+        path: "/events",
+        element: <Events/>,
+      },
+      {
+        path: "/support",
+        element: <Support/>,
+      },
+      {
+        path: "/blog",
+        element: <Blog/>,
+      },
+      {
+        path: "/privacy-policy",
+        element: <PrivacyPolicy/>,
+      },
+      {
+        path: "/terms-conditions",
+        element: <TermsConditions/>,
+      },
+      {
+        path: "*",
+        element: <Navigate to="/" replace/>
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}/>
+      <RouterProvider router={router}/>
+    <Provider/>
   </StrictMode>,
 )
